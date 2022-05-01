848e3abfc21bb7d0b1e18051e962367cceb7c81e
Federico Galan <fegv.2@hotmail.com.ar>
2022-05-01 14:30:42 -0300

    Se crea el Empty component con su respectivo SASS

---
+++ src/components/journal/Empty.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Empty = () => {
+  return (
+    <div className='empty__main-content'>
+
+        <p>
+            Select something
+            <br />
+            or create an entry
+        </p>
+        <i className='far fa-star fa-4x mt-5'/>
+
+    </div>
+  )
+}
