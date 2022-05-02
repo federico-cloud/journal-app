17bd4db24d86ca850ee6939473df4a28770e32db
Federico Galan <fegv.2@hotmail.com.ar>
2022-05-02 12:31:07 -0300

    Se corrigen importaciones

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { AppRouter } from './components/routers/AppRouter'
-import { store } from './store/store'
+import { store } from './components/store/store'
 
 export const JournalApp = () => {
   return (
