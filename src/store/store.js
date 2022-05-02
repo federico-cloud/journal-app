5436377d45e43608fcaeb5b7cbb871399f16f1de
Federico Galan <fegv.2@hotmail.com.ar>
2022-05-02 12:31:51 -0300

    Se corrige directorio

--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, combineReducers } from "redux";
-import { authReducer } from "../reducers/authReducer";
-
-const reducers = combineReducers({
-    auth: authReducer,
-
-});
-
-export const store = createStore(reducers);
