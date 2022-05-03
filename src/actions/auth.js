5df9f9278d57ffd9e90eb62308ddd5800013af37
Federico Galan <fegv.2@hotmail.com.ar>
2022-05-03 12:03:11 -0300

    Se aplica redux-thunk para procesos asincronos en la aplicacion

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,13 @@
 import { types } from '../types/types'
 
+export const authStart = (email, password) => {
+    return (dispatch) => {
+        setTimeout(() => {
+            dispatch(login(email, password));
+        }, 3000);
+    }
+};
+
 export const login = (uid, name) => ({
     type: types.login,
     payload: {
