ca40f797fe1828320f645147d476687597fb6b50
Federico Galan <fegv.2@hotmail.com.ar>
2022-05-05 11:37:54 -0300

    Se agrega un observador del estado para guardar la info del login

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { AuthRouter } from './AuthRouter';
 import { JournalScreen } from '../components/journal/JournalScreen';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase/firebase';
+import { useDispatch } from 'react-redux';
+import { login } from '../actions/auth';
 
 export const AppRouter = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth,({uid, displayName}) => {
+      if(uid){
+        dispatch(login(uid, displayName));
+      }
+    });
+  }, [dispatch])
+  
+
   return (
     <>
         <BrowserRouter>
